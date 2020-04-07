a9afe295ea61eff5f1314641d0e07744832a1637
fedorov <serstalk@gmail.com>
2020-04-08 00:43:55 +0300

    FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,21 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        System.out.println("");
+        fizzBuzz(9);
+        fizzBuzz(15);
+        fizzBuzz(20);
     }
+
+    public static void fizzBuzz(int input){
+
+        if ((input % 3 == 0) & (input % 5 == 0)){
+            System.out.println("FizzBuzz");
+        } else if (input % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (input % 5 == 0) {
+            System.out.println("Buzz");
+        }
+
+    }
+
+
 }
---
+++ src/Palindrom.java
@@ -0,0 +1,15 @@
+public class Palindrom {
+    public static void main(String[] args) {
+        checkPalindrom("Madam i’m Adam");
+
+    }
+
+    public static void checkPalindrom(String s){
+
+
+    }
+
+
+}
+
+
